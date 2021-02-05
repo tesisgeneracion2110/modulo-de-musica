2130535a4bbc59b5d50292c0ad486b0ab20674fc
Oscar Fonseca <ogfonseca9@gmail.com>
2021-02-05 00:55:24 -0500

    Configure coverage in workflow

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,11 @@
-from src.main import index
+import pytest
+from src.read_lyrics import delete_empty_lines
 
-def test_index():
-    assert index() == "Hello, world!"
+
+@pytest.mark.parametrize('data, result', [
+    ('first line\n\nsecond line\n', 'first line\nsecond line\n'),
+    ('first line\nsecond line\n', 'first line\nsecond line\n'),
+    ('', '')
+])
+def test_delete_empty_lines(data, result):
+    assert delete_empty_lines(data) == result

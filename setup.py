1d7d5a83d27f280d6418deaa087418e57ac68e74
Oscar Fonseca <ogfonseca9@gmail.com>
2021-03-19 15:14:22 -0500

    Change initial files

--- setup.py
+++ setup.py
@@ -1,11 +1,22 @@
 from setuptools import setup, find_packages
 
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license_file = f.read()
 
 setup(
-    name='src',
-    version='1.0.0',
+    name='sample',
+    version='0.1.0',
+    description='Project Module package',
+    long_description=readme,
+    author='tesisgeneracion2110',
+    author_email='fonseca.oscar@javeriana.edu.co',
+    url='https://github.com/tesisgeneracion2110/music-module',
+    license=license_file,
     install_requires=[
         'pytest'
     ],
-    packages=find_packages()
+    packages=find_packages(exclude=('tests', 'docs'))
 )

4b6dc54b171303071f85c7699045d5dec144fdf2
Oscar Fonseca <ogfonseca9@gmail.com>
2021-03-19 15:38:32 -0500

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,13 +4,3 @@ import pytest
 
 def test_index():
     assert sample.main.index() == "Hello, world!"
-
-
-@pytest.mark.parametrize('x, y, result', [
-    (10, 10, 20),
-    (5, 5, 10),
-    (6, 6, 12)
-])
-def test_add(x, y, result):
-    assert sample.main.add(x, y) == result
-

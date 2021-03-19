97875a602ec517276b76e3c51d34b19d19b58b60
Oscar Fonseca <ogfonseca9@gmail.com>
2021-03-19 15:38:19 -0500

    Create music package and song class

--- sample/main.py
+++ sample/main.py
@@ -1,6 +1,3 @@
 def index():
     return "Hello, world!"
 
-def add(x, y):
-    return x + y
-
---
+++ sample/music/songc.py
@@ -0,0 +1,6 @@
+class Song:
+    def __init__(self, bpm, root, scale, n_chords):
+        self.bpm = bpm
+        self.root = root
+        self.scale = scale
+        self.n_chords = n_chords
